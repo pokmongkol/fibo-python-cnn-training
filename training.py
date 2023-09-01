54eaed09a52f5fc37afa027241904dbaee11a9f4
pok_mongkol <tapok23@gmail.com>
2023-09-01 09:35:59 +0700

    first

---
+++ training.py
@@ -0,0 +1,63 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+
+
+# Assuming you have two folders: 'good' and 'bad', each containing their respective images
+
+# Load and preprocess data
+image_size = (1920, 1080)
+num_samples_per_class_1 = 20
+num_samples_per_class_0 = 20
+num_classes = 2
+
+# Placeholder arrays for images and labels
+images = np.zeros((num_samples_per_class_1+num_samples_per_class_0, image_size[0], image_size[1], 3))
+labels = np.zeros((num_samples_per_class_1+num_samples_per_class_0,))
+
+# Load 'good' images
+for i in range(num_samples_per_class_1):
+    # print(f'data/good/Good ({i+1}).jpg')
+    img = tf.keras.preprocessing.image.load_img(f'data/good/Good ({i+1}).jpg', target_size=image_size)
+    img_array = tf.keras.preprocessing.image.img_to_array(img)
+    images[i] = img_array
+    labels[i] = 1  # Good label
+
+# Load 'bad' images
+for i in range(num_samples_per_class_0):
+    # print(f'data/bad/Bad ({i+1}).jpg')
+    img = tf.keras.preprocessing.image.load_img(f'data/bad/Bad ({i+1}).jpg', target_size=image_size)
+    img_array = tf.keras.preprocessing.image.img_to_array(img)
+    images[i + num_samples_per_class_1] = img_array
+    labels[i + num_samples_per_class_1] = 0  # Bad label
+
+# print(images)
+# print(labels)
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.2, random_state=42)
+
+# Build the CNN model
+model = Sequential([
+    Conv2D(32, (3, 3), activation='relu', input_shape=(image_size[0], image_size[1], 3)),
+    MaxPooling2D((2, 2)),
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)),
+    Conv2D(128, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dropout(0.5),
+    Dense(1, activation='sigmoid')  # Use sigmoid for binary classification
+])
+
+# Compile the model
+model.compile(optimizer=Adam(learning_rate=0.001), loss='binary_crossentropy', metrics=['accuracy'])
+
+# Train the model
+model.fit(X_train, y_train, batch_size=16, epochs=10, validation_data=(X_test, y_test))
+
+print(model)
